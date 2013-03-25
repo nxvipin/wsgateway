85032fdd00832e89c7ba7d7d50c0aa83f4d07b6b
Vipin Nair <swvist@gmail.com>
2013-03-25 22:59:04 +0530

    Adding standard OTP application template

---
+++ src/wsgateway_app.erl
@@ -0,0 +1,16 @@
+-module(wsgateway_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    wsgateway_sup:start_link().
+
+stop(_State) ->
+    ok.
