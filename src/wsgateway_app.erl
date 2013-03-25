4432a24dc62073849427c9a16cab5416e52bc87a
Vipin Nair <swvist@gmail.com>
2013-03-26 00:16:26 +0530

    Trailing commas aren't tolerated

--- src/wsgateway_app.erl
+++ src/wsgateway_app.erl
@@ -12,7 +12,7 @@
 start(_StartType, _StartArgs) ->
 	Dispatch = cowboy_router:compile([
 		{'_', [
-			{"/websocket", ws_handler, []},
+			{"/websocket", ws_handler, []}
 		]}
 	]),
 	{ok, _} = cowboy:start_http(http, 100, [{port, 8080}], [
